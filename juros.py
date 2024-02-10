d0d27cd8af5385ee10083ef5ee73c3cd5c8483df
Eduardo Melo <eduardo87@gmail.com>
2024-02-10 13:33:31 -0300

    retirando comentário para mostrar cálculo mês a mês

--- juros.py
+++ juros.py
@@ -1,21 +1,22 @@
 from decimal import Decimal
 
-JUROS= 1.03
-APORTE_MENSAL = 100
+JUROS: float = 1.03
+APORTE_MENSAL: float = 100
 
-meses_investindo = 120
+meses_investindo: int = 120
 
-saldo_inicial = 0
+saldo_inicial: float = 0
 
-saldo_atual = 0
-saldo_sem_juros = 0
+saldo_atual: float = 0
+saldo_sem_juros: float = 0
 
-diferenca = 0
+diferenca: float = 0
 
-mes = 1
+mes: int = 1
 
 print('#################################')
-print(f'||| Taxa de juros de {JUROS} ao mês, aplicada durante {meses_investindo} mêses || ({meses_investindo / 12} anos)|||')
+print(f'||| Taxa de juros de {JUROS} ao mês, aplicada durante '
+      f'{meses_investindo} mêses || ({meses_investindo / 12} anos)|||')
 print(f'### Valor depositado por mês {APORTE_MENSAL}, começando com {saldo_inicial} de saldo ###')
 print('#################################')
 while mes <= meses_investindo:
@@ -23,9 +24,9 @@ while mes <= meses_investindo:
     saldo_sem_juros = saldo_sem_juros + APORTE_MENSAL
 
     diferenca = saldo_atual - saldo_sem_juros
+#
+    print(f'Mês|{mes}: Saldo com júros: {saldo_atual}' + f' | saldo sem juros : {saldo_sem_juros}' + f' | soma dos juros: {diferenca}')
+#
+    mes += 1
 
-    #print(f'|{mes}: {saldo_atual}' + f' | saldo sem juros :: {saldo_sem_juros}' + f' | diferença: {diferenca}')
-
-    mes+=1
-
-print(f'|{mes -1}: {Decimal(saldo_atual)}' + f' | saldo sem juros :: {saldo_sem_juros}' + f' | diferença: {diferenca}')
+print(f' Total | Com júros {mes -1}: R${Decimal(saldo_atual)}' + f' | sem juros: R${saldo_sem_juros}' + f' | soma dos júros : R${diferenca} |')
